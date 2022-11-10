d028879239e39471c89290b57a79b10aab02c067
Rasai Stewart <60219750+RasaiStewart@users.noreply.github.com>
2022-11-10 18:06:43 +0000

    App.py, dragon.py

---
+++ app.py
@@ -0,0 +1,32 @@
+from dragon import dragon, R
+from turtle import Turtle, Screen
+
+# Turtle setup
+turtle = Turtle("turtle")
+turtle.hideturtle()
+turtle.speed("fastest")
+turtle.color("#ff69aa")
+
+# Screen setup
+screen = Screen()
+screen.title("Dragon Curve")
+screen.bgcolor("black")
+screen.screensize(1920*3, 1080*3)
+screen.setup(width=1.0, height=1.0, startx=None, starty=None)
+
+
+# Draw
+LENGTH = 10
+turtle.forward(LENGTH)
+for element in dragon(17):
+    if element == R:
+        turtle.right(90)
+        turtle.forward(LENGTH)
+    else:
+        turtle.left(90)
+        turtle.forward(LENGTH)
+
+# When finished, click to exit
+turtle.color("white")
+turtle.write("click to exit", font=("Calibri", 16, "bold"))
+screen.exitonclick()
